2f1382a7e02d8491d5f4bd3a7f5d581c312abb7f
Chandara-Sin <chandara388588@gmail.com>
2022-10-20 17:01:00 +0700

    feat: add api key verify

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { json, Response, Router, urlencoded } from "express";
+import express, { json, NextFunction, Request, Response, Router, urlencoded } from "express";
 import { IAppDependencies } from ".";
 import { createUserHandler } from "./domain/user/user.handler";
 import { mwLogger } from "./logger";
+import { verifyAPIKey } from "./mw";
 
 const main = (dependencies: IAppDependencies) => {
   const app = express();
@@ -20,12 +21,19 @@ const main = (dependencies: IAppDependencies) => {
   app.use(mwLogger);
 
   const routes = Router();
-  routes.post("/users", createUserHandler(dependencies));
+  routes.post("/users", verifyAPIKey(), createUserHandler(dependencies));
 
   app.use("/api", routes);
   app.get("/api/healthz", (_, res: Response) => {
     res.status(200).json({ message: "Ok v1" });
   });
+
+  app.use((error: unknown, _: Request, res: Response, __: NextFunction) =>
+    res.status(500).json({
+      status_code: 500,
+      message: (error as Error).message,
+    })
+  );
 
   return app;
 };
