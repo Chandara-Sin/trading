c980e68dc3c9002d1488338cde8087f6937e117c
Chandara-Sin <chandara388588@gmail.com>
2022-10-20 17:01:16 +0700

    feat: add api key verify

---
+++ src/mw/index.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const verifyAPIKey = () => (req: Request, res: Response, next: NextFunction) => {
+  const apiKeyEncoded = Buffer.from(process.env.API_KEY_PUBLIC ?? "").toString("base64");
+  const apiKeyHeader = req.get("X-API-KEY");
+  next(
+    !apiKeyHeader
+      ? new Error("Missing API Key")
+      : apiKeyHeader !== apiKeyEncoded && new Error("API Key not correct")
+  );
+};
