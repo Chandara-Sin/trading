2d72bb9885479850088315e6dbf1d382669f1e41
Chandara-Sin <chandara388588@gmail.com>
2022-10-22 23:57:02 +0700

    feat: add get user api

--- src/domain/user/user.handler.ts
+++ src/domain/user/user.handler.ts
@@ -11,7 +11,24 @@ export const createUserHandler =
       .createUser(user)
       .then(rs => res.status(201).send(new UserModel(rs).toJson))
       .catch(err => {
-        logger.error("create err", err);
+        logger.error("create user err", err);
+        next(err);
+      });
+  };
+
+export const getUserHandler =
+  (userService: IUserService): Handler =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    userService
+      .getUser(id)
+      .then(rs =>
+        rs
+          ? res.status(200).json(new UserModel(rs).toJson)
+          : next(new Error(`User ID not found ${id}`))
+      )
+      .catch(err => {
+        logger.error("get user err", err);
         next(err);
       });
   };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { IAppDependencies } from "..";
-import { createUserHandler } from "../domain/user/user.handler";
+import { createUserHandler, getUserHandler } from "../domain/user/user.handler";
 import { verifyAPIKey } from "../mw";
 
 export const appRoutes = (dependencies: IAppDependencies) => (route: Router) => {
   route.post("/users", verifyAPIKey(), createUserHandler(dependencies.userService));
+  route.get("/users/:id", verifyAPIKey(), getUserHandler(dependencies.userService));
   return route;
 };
