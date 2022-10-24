84702dbf328f89467b9666e52d22789dbe029bd1
Chandara-Sin <chandara388588@gmail.com>
2022-10-24 22:10:23 +0700

    feat: add pagination

---
+++ src/pagination/index.ts
@@ -0,0 +1,23 @@
+import { Prisma } from "../../src/generated/client";
+
+export interface IPaginationParams {
+  page: number;
+  rows: number;
+  sort?: string;
+  direction?: string;
+  search?: string;
+}
+
+export interface IPaginationRes {
+  total: number;
+  rows: number;
+  page: number;
+  data: {}[];
+}
+
+export const getRows = (rows?: string) => (rows ? +rows : 10);
+
+export const getPage = (page?: string) => (page ? +page : 1);
+
+export const getDirection = (dir?: string) =>
+  dir ? (dir as Prisma.SortOrder) : Prisma.SortOrder.asc;
