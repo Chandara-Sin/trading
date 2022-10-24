0026becc78ad649f6c39d8490a1959cbf018b41b
Chandara-Sin <chandara388588@gmail.com>
2022-10-24 22:12:36 +0700

    test: add mock get user list service

--- tests/unit-tests/domain/mock.user.service.ts
+++ tests/unit-tests/domain/mock.user.service.ts
@@ -1,12 +1,14 @@
 import { reqUser } from "../../../src/domain/user/user";
 import { IUserService } from "../../../src/domain/user/user.service";
 import { User } from "../../../src/generated/client";
+import { IPaginationParams } from "../../../src/pagination";
 
 export class MockUserService implements IUserService {
   createUser: (user: reqUser) => Promise<User>;
   getUser: (id: string) => Promise<User | null>;
   updateUser: (user: Pick<reqUser, "first_name" | "last_name"> & { id: string }) => Promise<User>;
   deleteUser: (id: string) => Promise<User>;
+  getUserList: (pag: IPaginationParams) => Promise<{ data: User[]; total: number }>;
 
   constructor(user: User) {
     this.createUser = jest.fn().mockImplementation(() =>
@@ -18,5 +20,6 @@ export class MockUserService implements IUserService {
     this.getUser = jest.fn();
     this.updateUser = jest.fn();
     this.deleteUser = jest.fn();
+    this.getUserList = jest.fn();
   }
 }
